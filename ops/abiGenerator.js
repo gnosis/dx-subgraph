1910abd023be93ba7a48f3283247e7ba74efff00
InfiniteStyles <AntonVS.Professional@gmail.com>
2019-05-06 11:34:11 -0700

    Setup exoskeleton. Graph Codegen and Graph Build running.

--- ops/abiGenerator.js
+++ ops/abiGenerator.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 
 const contract = JSON.parse(
-  fs.readFileSync(
-    'node_modules/@gnosis.pm/hg-contracts/build/contracts/PredictionMarketSystem.json',
-    'utf8'
-  )
+  fs.readFileSync('node_modules/@gnosis.pm/dx-contracts/build/contracts/DutchExchange.json', 'utf8')
 );
 
-fs.writeFileSync('./abis/PredictionMarketSystem.json', JSON.stringify(contract.abi));
+fs.writeFileSync('./abis/DutchExchange.json', JSON.stringify(contract.abi));
 console.log(JSON.stringify(contract.abi));
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,23 @@
-import { NewGravatar, UpdatedGravatar } from './types/Gravity/Gravity'
-import { Gravatar } from './types/schema'
-
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
-
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+import { crypto, Address, BigInt, Bytes, TypedMap, ByteArray } from '@graphprotocol/graph-ts';
+import {
+  NewDeposit,
+  NewWithdrawal,
+  NewSellOrder,
+  NewBuyOrder,
+  NewSellerFundsClaim,
+  NewBuyerFundsClaim,
+  NewTokenPair,
+  AuctionCleared,
+  AuctionStartScheduled,
+  Fee
+} from './types/DutchExchange/DutchExchange';
+import {
+  Auction,
+  TokenPair,
+  Trader,
+  SellOrder,
+  BuyOrder,
+  Token,
+  TokenBalance,
+  TokenAuctionBalance
+} from './types/schema';
