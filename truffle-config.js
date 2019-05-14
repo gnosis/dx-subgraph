d6a7d4b95cfb4745056c534da6a5d1fe5b61272e
Anxo Rodriguez <anxolin@gmail.com>
2019-05-14 18:26:46 +0200

    Add truffle config and migrate DutchX

--- truffle-config.js
+++ truffle-config.js
@@ -1,96 +1,107 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+const assert = require('assert');
+const HDWalletProvider = require('truffle-hdwallet-provider')
+;
+const GAS_PRICE_GWEI = process.env.GAS_PRICE_GWEI || 5;
+const GAS_LIMIT = 6.5e6;
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const DEFAULT_MNEMONIC =
+  'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
 
-module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+// Load env vars
+require('dotenv').config();
 
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-      host: '127.0.0.1', // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: '*' // Any network (default: none)
-    }
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+// Get the mnemonic
+const privateKey = process.env.PK;
+let mnemonic = process.env.MNEMONIC;
+if (!privateKey && !mnemonic) {
+  mnemonic = DEFAULT_MNEMONIC;
+}
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
+const infuraProjectId = process.env.INFURA_KEY || '9408f47dedf04716a03ef994182cf150';
+function truffleConfig({
+  mnemonic = DEFAULT_MNEMONIC,
+  privateKey,
+  gasPriceGWei = GAS_PRICE_GWEI,
+  gas = GAS_LIMIT,
+  optimizedEnabled = true,
+  urlRinkeby = `https://rinkeby.infura.io/v3/${  infuraProjectId}`,
+  urlKovan = `https://kovan.infura.io/v3/${  infuraProjectId}`,
+  urlMainnet = `https://mainnet.infura.io/v3/${  infuraProjectId}`,
+  urlDevelopment = 'localhost',
+  portDevelopment = 8545
+} = {}) {
+  assert(mnemonic, 'The mnemonic has not been provided');
+  console.log(`Using gas limit: ${gas / 1000} K`);
+  console.log(`Using gas price: ${gasPriceGWei} Gwei`);
+  console.log(`Optimizer enabled: ${optimizedEnabled}`);
+  console.log('Using default mnemonic: %s', mnemonic === DEFAULT_MNEMONIC);
+  const gasPrice = gasPriceGWei * 1e9;
 
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: '0.5.2' // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
+  let _getProvider;
+  if (privateKey) {
+    console.log('Using private key');
+    _getProvider = url => {
+      return () => {
+        assert(infuraProjectId, 'Need an infura ProjectID. INFURA_KEY env var');
+        return new HDWalletProvider([privateKey], url);
+      };
+    };
+  } else {
+    console.log(mnemonic === DEFAULT_MNEMONIC ? 'Using default mnemonic' : 'Using custom mnemonic');
+    _getProvider = url => {
+      return () => {
+        assert(infuraProjectId, 'Need an infura ProjectID. INFURA_KEY env var');
+        return new HDWalletProvider(mnemonic, url);
+      };
+    };
   }
-};
+
+  return {
+    networks: {
+      development: {
+        host: process.env.RPC_URL || urlDevelopment,
+        port: portDevelopment,
+        gas,
+        gasPrice,
+        network_id: '*'
+      },
+      mainnet: {
+        provider: _getProvider(urlMainnet),
+        network_id: '1',
+        gas,
+        gasPrice
+      },
+      rinkeby: {
+        provider: _getProvider(urlRinkeby),
+        network_id: '4',
+        gas,
+        gasPrice
+      },
+      kovan: {
+        provider: _getProvider(urlKovan),
+        network_id: '42',
+        gas,
+        gasPrice
+      }
+    },
+    compilers: {
+      solc: {
+        version: '0.5.2',
+        docker: process.env.SOLC_USE_DOCKER === 'true' || false,
+        settings: {
+          optimizer: {
+            enabled: optimizedEnabled, // Default: false
+            runs: 200
+          }
+          // evmVersion: "byzantium"  // Default: "byzantium". Others:  "homestead", ...
+        }
+      }
+    }
+  };
+}
+
+module.exports = truffleConfig({
+  optimizedEnabled: true,
+  mnemonic,
+  privateKey
+});
